afc156ffdf95d545747764b8b508f4f500f5c59f
azarJY <143927567+azarJY@users.noreply.github.com>
2024-02-28 03:16:07 +0000

    [使用記録createアクションエラー要因判明→次回対処]

--- app/controllers/farmer/records_controller.rb
+++ app/controllers/farmer/records_controller.rb
@@ -17,25 +17,26 @@ class Farmer::RecordsController < ApplicationController
     @record = Record.new(record_params)
 
     if params[:record][:farmland_id].present?
-        selected_farmland = current_farmer.farmlands.find(params[:record][:farmland_id])
-        @record.farmland = selected_farmland
-        @record.farmland.id =selected_farmland.id
-        @record.farmland.farmland = selected_farmland.farmland
+      selected_farmland = current_farmer.farmlands.find(params[:record][:farmland_id])
+      @record.farmland.id = selected_farmland.id
     end
 
     if params[:record][:pesticide_id].present?
       selected_pesticide = current_farmer.pesticides.find(params[:record][:pesticide_id])
-      @record.pesticide = selected_pesticide
       @record.pesticide.id = selected_pesticide.id
       @record.name = selected_pesticide.name
       @record.subject = selected_pesticide.subject
       @record.code = selected_pesticide.code
     end
+    @record.day = params[:record][:day]
   end
 
   def create
     @farmer = Farmer.find(current_farmer.id)
     @record = current_farmer.records.new(record_params)
+    @record.farmland_id = params[:record][:farmland_id]
+
+    Rails.logger.debug params[:record][:day]
 
     if @record.save
       flash[:notice] = "農薬使用記録の作成に成功しました。"
@@ -49,24 +50,24 @@ class Farmer::RecordsController < ApplicationController
 
   def show
     @farmer = Farmer.find(current_farmer.id)
-    @record = Record.find(params[:id])
+    @record = current_farmer.records.find(params[:id])
   end
 
   def edit
     @farmer = Farmer.find(current_farmer.id)
-    @record = Record.find(params[:id])
+    @record = current_farmer.records.find(params[:id])
   end
 
   def update
     @farmer = Farmer.find(current_farmer.id)
-    @record = Record.find(params[:id])
+    @record = current_farmer.records.find(params[:id])
     record.update(record.params)
     redirect_to farmer_records_path
   end
 
   def destroy
     @farmer = Farmer.find(current_farmer.id)
-    @record = Record.find(params[:id])
+    @record = current_farmer.records.find(params[:id])
     record.destroy
     redirect_to farmer_records_path
   end
