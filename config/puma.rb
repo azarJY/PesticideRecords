785004e47627ebfa3ca3c57e5c177aac7c21745d
azarJY <143927567+azarJY@users.noreply.github.com>
2024-03-18 02:05:58 +0000

    [puma修正3パス見直し]

--- config/puma.rb
+++ config/puma.rb
@@ -43,14 +43,14 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
 
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-rails_root = Dir.pwd
+
 # 本番環境のみデーモン起動
 if Rails.env.production?
-  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
-  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  pidfile File.join(Rails.root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(Rails.root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log'),
-    File.join(rails_root, 'log', 'puma-error.log'),
+    File.join(Rails.root, 'log', 'puma.log'),
+    File.join(Rails.root, 'log', 'puma-error.log'),
     true
   )
   # デーモン
