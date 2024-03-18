af1f991d4ebbb67180c00d43b160960937133022
azarJY <143927567+azarJY@users.noreply.github.com>
2024-03-18 01:12:22 +0000

    [細部修正2]

--- app/controllers/farmer/confirmations_controller.rb
+++ app/controllers/farmer/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Farmers::ConfirmationsController < Devise::ConfirmationsController
+class Farmer::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
