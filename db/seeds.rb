68505ef83ef1ba60110454e001f697eb2473fbe2
azarJY <143927567+azarJY@users.noreply.github.com>
2024-05-21 12:25:08 +0000

    [2024/05/21 seeds.rb修正]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,8 @@
 # ゲストログイン用アカウント
 # 既存のゲストユーザーを特定して更新する
 guest_user = Farmer.find_by(email: "noukou@gmail.com")
-if guest_user
-  guest_user.update(
-    password: "156464",
-    last_name: "見本",
-    first_name: "太郎",
-    last_name_kana: "ミホン",
-    first_name_kana: "タロウ",
-    farm_name: "サンプル農場",
-    crop: "夏おとめ",
-  )
-else
-  # 存在しない場合は新しいユーザーを作成する
-  Farmer.create!(
+unless guest_user
+  guest_user = Farmer.create!(
     email: "noukou@gmail.com",
     password: "156464",
     last_name: "見本",
@@ -21,55 +10,68 @@ else
     last_name_kana: "ミホン",
     first_name_kana: "タロウ",
     farm_name: "サンプル農場",
-    crop: "夏おとめ",
+    crop: "夏おとめ"
+  )
+else
+  guest_user.update(
+    password: "156464",
+    last_name: "見本",
+    first_name: "太郎",
+    last_name_kana: "ミホン",
+    first_name_kana: "タロウ",
+    farm_name: "サンプル農場",
+    crop: "夏おとめ"
   )
 end
 
-Farmland.create!(
+# Farmlandの作成
+farmland_a = Farmland.create!(
   farmer_id: guest_user.id,
   land: "単棟ハウスA",
   area: "50m×4",
-  overview: "説明を記入 例:冷熱設備有イチゴ用",
+  overview: "説明を記入 例:冷熱設備有イチゴ用"
 )
 
-Farmland.create!(
+farmland_b = Farmland.create!(
   farmer_id: guest_user.id,
   land: "単棟ハウスB",
   area: "50m×4",
-  overview: "説明を記入 例:冷熱設備有イチゴ用",
+  overview: "説明を記入 例:冷熱設備有イチゴ用"
 )
 
-Farmland.create!(
+farmland_c = Farmland.create!(
   farmer_id: guest_user.id,
   land: "単棟ハウスC",
   area: "50m×4",
-  overview: "説明を記入 例:冷熱設備有イチゴ用",
+  overview: "説明を記入 例:冷熱設備有イチゴ用"
 )
 
-Pesticide.create!(
+# Pesticideの作成
+pesticide_1 = Pesticide.create!(
   farmer_id: guest_user.id,
   name: "農薬1",
   code: "殺虫5",
   subject: "アザミウマ・ハダニ",
   use_method: "希釈散布",
   magnification: "2000倍",
-  usable_number: 3,
+  usable_number: 3
 )
 
-Pesticide.create!(
+pesticide_2 = Pesticide.create!(
   farmer_id: guest_user.id,
   name: "農薬2",
   code: "無し",
   subject: "うどんこ病",
   use_method: "希釈散布",
   magnification: "2000倍",
-  usable_number: 10,
+  usable_number: 10
 )
 
+# Recordの作成
 Record.create!(
   farmer_id: guest_user.id,
-  farmland_id: Farmland.first.id,
-  pesticide_id: Pesticide.first.id,
+  farmland_id: farmland_a.id,
+  pesticide_id: pesticide_1.id,
   day: Date.new(2024, 4, 1),
   name: "農薬1",
   subject: "アザミウマ・ハダニ",
@@ -78,13 +80,13 @@ Record.create!(
   amount: "250ml",
   water: "500L",
   user: "サンプルユーザー",
-  confimer: "サンプル承認者",
+  confimer: "サンプル承認者"
 )
 
 Record.create!(
   farmer_id: guest_user.id,
-  farmland_id: Farmland.last.id,
-  pesticide_id: Pesticide.first.id,
+  farmland_id: farmland_b.id,
+  pesticide_id: pesticide_1.id,
   day: Date.new(2024, 4, 1),
   name: "農薬1",
   subject: "アザミウマ・ハダニ",
@@ -93,13 +95,13 @@ Record.create!(
   amount: "250ml",
   water: "500L",
   user: "サンプルユーザー",
-  confimer: "サンプル承認者",
+  confimer: "サンプル承認者"
 )
 
 Record.create!(
   farmer_id: guest_user.id,
-  farmland_id: Farmland.first.id,
-  pesticide_id: Pesticide.first.id,
+  farmland_id: farmland_a.id,
+  pesticide_id: pesticide_1.id,
   day: Date.new(2024, 4, 5),
   name: "農薬1",
   subject: "アザミウマ・ハダニ",
@@ -108,13 +110,13 @@ Record.create!(
   amount: "250ml",
   water: "500L",
   user: "サンプルユーザー",
-  confimer: "サンプル承認者",
+  confimer: "サンプル承認者"
 )
 
 Record.create!(
   farmer_id: guest_user.id,
-  farmland_id: Farmland.first.id,
-  pesticide_id: Pesticide.first.id,
+  farmland_id: farmland_a.id,
+  pesticide_id: pesticide_1.id,
   day: Date.new(2025, 1, 1),
   name: "農薬1",
   subject: "アザミウマ・ハダニ",
@@ -123,14 +125,15 @@ Record.create!(
   amount: "250ml",
   water: "500L",
   user: "サンプルユーザー",
-  confimer: "サンプル承認者",
+  confimer: "サンプル承認者"
 )
 
 Season.create!(
   farmer_id: guest_user.id,
   start_date: Date.new(2024, 4, 1),
-  end_date: Date.new(2024, 12, 31),
+  end_date: Date.new(2024, 12, 31)
 )
+
 
 
 
